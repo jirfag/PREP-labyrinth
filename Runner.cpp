9c9c31212002d201431190b42f94cd895094d3e8
Oleg Utkin <oleg_utkin@me.com>
2016-11-10 22:26:28 +0300

    Test commit

--- Runner.cpp
+++ Runner.cpp
@@ -6,11 +6,23 @@
 #include <vector>
 #include <cstdlib>
 #include "utils.hpp"
+#include <ctime>
+
+Runner::Runner() {
+    srand(time(0));
+}
 
 Direction Runner::step()
 {
     // TODO: you need to replace the code below with your implementation of labyrinth solver.
     // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
+    const std::vector<Direction> directions = {
+        Direction::UP,
+        Direction::DOWN,
+        Direction::LEFT,
+        Direction::RIGHT
+    };
+
+
     return directions[std::rand() % directions.size()];
 }
