903d774275a503f4dcb269eb9422b8fd738010f5
barzug <barsukof@live.com>
2016-11-10 20:51:11 +0300

    case or if?

--- Runner.cpp
+++ Runner.cpp
@@ -76,7 +76,7 @@ Direction Runner::step() {
         return Direction::DOWN;
     }
     //по очереди проверять свободен ли проход справа и ходил ли я в него YJ RFR 'NJ CLTKFNM&?
-
+/*
     if ( PrevStep == Direction::UP ) {
         if ( current_status.left == BlockType::FREE ) {
             PrevStep = Direction::LEFT;
@@ -132,6 +132,69 @@ Direction Runner::step() {
         } else {
             PrevStep = Direction::LEFT;
             return Direction::LEFT;
+        }
+    } */
+
+    switch ( PrevStep ) {
+        case Direction::UP: {
+            if ( current_status.left == BlockType::FREE ) {
+                PrevStep = Direction::LEFT;
+                return Direction::LEFT;
+            } else if ( current_status.up == BlockType::FREE ) {
+                PrevStep = Direction::UP;
+                return Direction::UP;
+            } else if ( current_status.right == BlockType::FREE ) {
+                PrevStep = Direction::RIGHT;
+                return Direction::RIGHT;
+            } else {
+                PrevStep = Direction::DOWN;
+                return Direction::DOWN;
+            }
+        }
+        case Direction::LEFT: {
+            if ( current_status.down == BlockType::FREE ) {
+                PrevStep = Direction::DOWN;
+                return Direction::DOWN;
+            } else if ( current_status.left == BlockType::FREE ) {
+                PrevStep = Direction::LEFT;
+                return Direction::LEFT;
+            } else if ( current_status.up == BlockType::FREE ) {
+                PrevStep = Direction::UP;
+                return Direction::UP;
+            } else {
+                PrevStep = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+        }
+        case Direction::DOWN: {
+            if ( current_status.right == BlockType::FREE ) {
+                PrevStep = Direction::RIGHT;
+                return Direction::RIGHT;
+            } else if ( current_status.down == BlockType::FREE ) {
+                PrevStep = Direction::DOWN;
+                return Direction::DOWN;
+            } else if ( current_status.left == BlockType::FREE ) {
+                PrevStep = Direction::LEFT;
+                return Direction::LEFT;
+            } else {
+                PrevStep = Direction::UP;
+                return Direction::UP;
+            }
+        }
+        default: {
+            if ( current_status.up == BlockType::FREE ) {
+                PrevStep = Direction::UP;
+                return Direction::UP;
+            } else if ( current_status.right == BlockType::FREE ) {
+                PrevStep = Direction::RIGHT;
+                return Direction::RIGHT;
+            } else if ( current_status.down == BlockType::FREE ) {
+                PrevStep = Direction::DOWN;
+                return Direction::DOWN;
+            } else {
+                PrevStep = Direction::LEFT;
+                return Direction::LEFT;
+            }
         }
     }
 }
