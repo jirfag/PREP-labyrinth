d7b2430b5578aef01351f655a4efb4cf87060d38
zwirec <zwirec@icloud.com>
2016-11-10 21:15:56 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -1,64 +1,32 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
-#include <stack>
-#include <list>
-#include <algorithm>
 
 #include "RunnerBase.hpp"
 
-class Cell{
-public:
-    // Constructors & Destructors
-    Cell();
-    Cell(const Status& state, const Direction& prevStep);
-    Cell(const Status& state);
-
-    bool isDeadlock() const;
-    Direction getBackDirection() const;
-
-    Direction chooseNextDirection() const;
-
-    bool getDirectionState(const Direction&) const;
-    void setDirectionState(const Direction&, bool value);
-
-private:
-    bool leftDone;
-    bool rightDone;
-    bool upDone;
-    bool downDone;
-
-    Status state;
-
-    bool isStart;
-
-    Direction prevStep;
-    Direction backDirection;
-};
-
-Direction getOppositeDirection(const Direction& direction);
-
 class Runner: public RunnerBase {
 public:
-    // Constructors
-    Runner();
-
+    Runner() : currDirection(Direction::RIGHT){};
     Direction step();
+    bool isFreeUp();
+    bool isFreeDown();
+    bool isFreeLeft();
+    bool isFreeRight();
+    Direction nowDirectionU();
+    Direction nowDirectionR();
+    Direction nowDirectionD();
+    Direction nowDirectionL();
+    Direction nowUDirectionLEFT();
+    Direction nowRDirectionLEFT();
+    Direction nowDDirectionLEFT();
+    Direction nowLDirectionLEFT();
 
+    bool isExitLeft();
+    bool isExitDown();
+    bool isExitUp();
+    bool isExitRight();
 private:
-    std::stack<Cell> history;
-    std::list<std::pair<int, int>> deadlocks;
-
-    Direction lastChoice;
-    bool isForwardDirection;
-
-    int x;
-    int y;
-
-    bool checkForDeadlock(int x, int y) const;
-    bool checkForDeadlock(const Direction&) const;
-
-    void addDeadlock(int x, int y);
+    Direction currDirection;
 };
 
 
