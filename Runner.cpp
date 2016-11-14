1394b7fc2c0bb564d173ae5fdc6c9f9930a43c7a
barzug <barsukof@live.com>
2016-11-14 13:18:51 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -4,26 +4,15 @@ Direction PrevStep = Direction::UP;
 
 Direction Runner::step() {
     switch ( PrevStep ) {
-        case Direction::RIGHT: {
-            if ( ( current_status.up == BlockType::FREE ) | ( current_status.up == BlockType::EXIT ) ) {
+        case Direction::UP: {
+            if ( ( current_status.left == BlockType::FREE ) | ( current_status.left == BlockType::EXIT ) ) {
+                return PrevStep = Direction::LEFT;
+            } else if ( ( current_status.up == BlockType::FREE ) | ( current_status.up == BlockType::EXIT ) ) {
                 return PrevStep = Direction::UP;
             } else if ( ( current_status.right == BlockType::FREE ) | ( current_status.right == BlockType::EXIT ) ) {
                 return PrevStep = Direction::RIGHT;
-            } else if ( ( current_status.down == BlockType::FREE ) | ( current_status.down == BlockType::EXIT ) ) {
-                return PrevStep = Direction::DOWN;
             } else {
-                return PrevStep = Direction::LEFT;
-            }
-        }
-        case Direction::DOWN: {
-            if ( ( current_status.right == BlockType::FREE ) | ( current_status.right == BlockType::EXIT ) ) {
-                return PrevStep = Direction::RIGHT;
-            } else if ( ( current_status.down == BlockType::FREE ) | ( current_status.down == BlockType::EXIT ) ) {
                 return PrevStep = Direction::DOWN;
-            } else if ( ( current_status.left == BlockType::FREE ) | ( current_status.left == BlockType::EXIT ) ) {
-                return PrevStep = Direction::LEFT;
-            } else {
-                return PrevStep = Direction::UP;
             }
         }
         case Direction::LEFT: {
@@ -37,15 +26,26 @@ Direction Runner::step() {
                 return PrevStep = Direction::RIGHT;
             }
         }
-        default: {
-            if ( ( current_status.left == BlockType::FREE ) | ( current_status.left == BlockType::EXIT ) ) {
+        case Direction::DOWN: {
+            if ( ( current_status.right == BlockType::FREE ) | ( current_status.right == BlockType::EXIT ) ) {
+                return PrevStep = Direction::RIGHT;
+            } else if ( ( current_status.down == BlockType::FREE ) | ( current_status.down == BlockType::EXIT ) ) {
+                return PrevStep = Direction::DOWN;
+            } else if ( ( current_status.left == BlockType::FREE ) | ( current_status.left == BlockType::EXIT ) ) {
                 return PrevStep = Direction::LEFT;
-            } else if ( ( current_status.up == BlockType::FREE ) | ( current_status.up == BlockType::EXIT ) ) {
+            } else {
+                return PrevStep = Direction::UP;
+            }
+        }
+        default: {
+            if ( ( current_status.up == BlockType::FREE ) | ( current_status.up == BlockType::EXIT ) ) {
                 return PrevStep = Direction::UP;
             } else if ( ( current_status.right == BlockType::FREE ) | ( current_status.right == BlockType::EXIT ) ) {
                 return PrevStep = Direction::RIGHT;
-            } else {
+            } else if ( ( current_status.down == BlockType::FREE ) | ( current_status.down == BlockType::EXIT ) ) {
                 return PrevStep = Direction::DOWN;
+            } else {
+                return PrevStep = Direction::LEFT;
             }
         }
     }
