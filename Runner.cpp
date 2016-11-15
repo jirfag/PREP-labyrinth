be1b3cccff22aed419dfdbf8d4525187d8d0f127
Oleg Utkin <oleg_utkin@me.com>
2016-11-15 22:26:51 +0300

    Revert "Add forward direction check"
    This reverts commit 4ec37747f75207fd0652c36f4c21cf24a36b0a84.

--- Runner.cpp
+++ Runner.cpp
@@ -18,9 +18,6 @@ Direction Runner::step() {
     switch (currentDirection) {
 
     case Direction::UP:
-        if (current_status.left == BlockType::WALL && (current_status.up == BlockType::FREE || current_status.up == BlockType::ENTER))
-            return Direction::UP;
-
         forward = current_status.up;
         left = current_status.left;
         right = current_status.right;
@@ -33,9 +30,6 @@ Direction Runner::step() {
         break;
 
     case Direction::DOWN:
-        if (current_status.right == BlockType::WALL && (current_status.down == BlockType::FREE || current_status.down == BlockType::ENTER))
-            return Direction::DOWN;
-
         forward = current_status.down;
         left = current_status.right;
         right = current_status.left;
@@ -47,9 +41,6 @@ Direction Runner::step() {
         break;
 
     case Direction::LEFT:
-        if (current_status.down == BlockType::WALL && (current_status.left == BlockType::FREE || current_status.left == BlockType::ENTER))
-            return Direction::LEFT;
-
         forward = current_status.left;
         left = current_status.down;
         right = current_status.up;
@@ -62,9 +53,6 @@ Direction Runner::step() {
         break;
 
     case Direction::RIGHT:
-        if (current_status.up == BlockType::WALL && (current_status.right == BlockType::FREE || current_status.right == BlockType::ENTER))
-            return Direction::RIGHT;
-
         forward = current_status.right;
         left = current_status.up;
         right = current_status.down;
