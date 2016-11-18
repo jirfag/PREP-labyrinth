965bf2b953ba949fedd6fe742a5d7edb2d1f753d
EvgeniaNevtrinosova <jenushkanor@gmail.com>
2016-11-18 20:41:11 +0400

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -1,16 +1,117 @@
-//
-// Created by tsv on 09.05.16.
-//
-
 #include "Runner.hpp"
-#include <vector>
-#include <cstdlib>
-#include "utils.hpp"
 
-Direction Runner::step()
-{
-    // TODO: you need to replace the code below with your implementation of labyrinth solver.
-    // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-    return directions[std::rand() % directions.size()];
+Direction Runner::step() {
+
+    if (current_status.up == BlockType::EXIT) {
+        return Direction::UP;
+    }
+    if (current_status.left == BlockType::EXIT) {
+        return Direction::LEFT;
+    }
+    if (current_status.right == BlockType::EXIT) {
+        return Direction::RIGHT;
+    }
+    if (current_status.down == BlockType::EXIT) {
+        return Direction::DOWN;
+    }
+
+    if (prevStep == Direction::LEFT){
+
+        if (current_status.down == BlockType::FREE){
+            prevStep = Direction::DOWN;
+            return Direction::DOWN;
+        }
+
+        if (current_status.left == BlockType::FREE){
+            prevStep = Direction::LEFT;
+            return Direction::LEFT;
+        }
+
+        if (current_status.up == BlockType::FREE){
+            prevStep = Direction::UP;
+            return Direction::UP;
+        }
+
+        if (current_status.right == BlockType::FREE){
+            prevStep = Direction::RIGHT;
+            return Direction::RIGHT;
+
+        }
+    }
+
+    if (prevStep == Direction::UP) {
+
+        if (current_status.left == BlockType::FREE){
+            prevStep = Direction::LEFT;
+            return Direction::LEFT;
+        }
+
+        if (current_status.up == BlockType::FREE){
+            prevStep = Direction::UP;
+            return Direction::UP;
+        }
+
+        if (current_status.right == BlockType::FREE){
+            prevStep = Direction::RIGHT;
+            return Direction::RIGHT;
+        }
+
+        if (current_status.down == BlockType::FREE){
+            prevStep = Direction::DOWN;
+            return Direction::DOWN;
+        }
+    }
+
+
+
+    if (prevStep == Direction::RIGHT){
+        if (current_status.up == BlockType::FREE){
+            prevStep = Direction::UP;
+            return Direction::UP;
+        }
+
+        if (current_status.right == BlockType::FREE){
+            prevStep = Direction::RIGHT;
+            return Direction::RIGHT;
+
+        }
+
+        if (current_status.down == BlockType::FREE){
+            prevStep = Direction::DOWN;
+            return Direction::DOWN;
+        }
+
+        if (current_status.left == BlockType::FREE){
+            prevStep = Direction::LEFT;
+            return Direction::LEFT;
+        }
+    }
+
+
+
+
+    if (prevStep == Direction::DOWN) {
+        if (current_status.right == BlockType::FREE){
+            prevStep = Direction::RIGHT;
+            return Direction::RIGHT;
+        }
+
+        if (current_status.down == BlockType::FREE){
+            prevStep = Direction::DOWN;
+            return Direction::DOWN;
+        }
+        if (current_status.left == BlockType::FREE){
+            prevStep = Direction::LEFT;
+            return Direction::LEFT;
+        }
+
+
+
+        if (current_status.up == BlockType::FREE){
+            prevStep = Direction::UP;
+            return Direction::UP;
+        }
+    }
+
+return prevStep;
 }
