2e3813af5be74327c3a6ab1bd46db3ab92ecab73
zwirec <zwirec@icloud.com>
2016-11-22 00:42:58 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -6,74 +6,83 @@
 
 Direction Runner::step() {
   
-    if (currDirection == Direction::UP) {
-        if (current_status.up == BlockType::EXIT)
+   if (currDirection == 0) {
+        if ((current_status.up == BlockType::EXIT))
             return Direction::UP;
 
         if (current_status.right == BlockType::EXIT)
             return Direction::RIGHT;
 
+        if(current_status.left == BlockType::EXIT)
+            return Direction::LEFT;
+
         if (current_status.left == BlockType::FREE)
-            return currDirection = Direction::LEFT;
+            return currDirection = 1, Direction::LEFT;
 
         if (current_status.up == BlockType::FREE)
             return Direction::UP;
 
         if (current_status.right == BlockType::FREE)
-            return currDirection = Direction::RIGHT;
+            return currDirection = 3, Direction::RIGHT;
 
-        return currDirection = Direction::DOWN;
+        return currDirection = 2, Direction::DOWN;
     }
-        if (currDirection == Direction::DOWN) {
+        if (currDirection == 2) {
             if (current_status.down == BlockType::EXIT)
                 return Direction::DOWN;
             if (current_status.right == BlockType::EXIT)
                 return Direction::RIGHT;
+            if(current_status.left == BlockType::EXIT)
+                return Direction::LEFT;
 
             if (current_status.right == BlockType::FREE)
-                return currDirection = Direction::RIGHT;
+                return currDirection = 3, Direction::RIGHT;
 
             if (current_status.down == BlockType::FREE)
-                return currDirection = Direction::DOWN;
+                return currDirection = 2, Direction::DOWN;
 
             if (current_status.left == BlockType::FREE)
-                return currDirection = Direction::LEFT;
+                return currDirection = 1, Direction::LEFT;
 
-                return currDirection = Direction::UP;
+                return currDirection = 0, Direction::UP;
         }
-        if (currDirection == Direction::LEFT) {
+        if (currDirection == 1) {
             if (current_status.up == BlockType::EXIT)
                 return Direction::UP;
             if (current_status.left == BlockType::EXIT)
                 return Direction::LEFT;
+            if(current_status.down == BlockType::EXIT)
+                return Direction::LEFT;
 
             if (current_status.down == BlockType::FREE)
-                return currDirection = Direction::DOWN;
+                return currDirection = 2, Direction::DOWN;
 
             if (current_status.left == BlockType::FREE)
-                return currDirection = Direction::LEFT;
+                return currDirection = 1, Direction::LEFT;
 
             if (current_status.up == BlockType::FREE)
-                return currDirection = Direction::UP;
+                return currDirection = 0, Direction::UP;
 
-                return currDirection = Direction::RIGHT;
+                return currDirection = 2, Direction::RIGHT;
         }
             if (current_status.up == BlockType::EXIT)
                 return Direction::UP;
             if (current_status.right == BlockType::EXIT)
                 return Direction::RIGHT;
+            if (current_status.down == BlockType::EXIT)
+                return Direction::LEFT;
 
             if (current_status.up == BlockType::FREE)
-                return currDirection = Direction::UP;
+                return currDirection = 0, Direction::UP;
 
             if (current_status.right == BlockType::FREE)
                 return Direction::RIGHT;
 
             if (current_status.down == BlockType::FREE)
-                return currDirection = Direction::DOWN;
+                return currDirection = 2, Direction::DOWN;
 
             if (current_status.left == BlockType::FREE)
-                return currDirection = Direction::LEFT;
+                return currDirection = 1, Direction::LEFT;
 
-    return currDirection;
+    return Direction::LEFT;
 }
