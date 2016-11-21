bfc6fc646255db245b3723ad7a0a1a0f59a0159c
zwirec <zwirec@icloud.com>
2016-11-21 23:41:36 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -5,17 +5,7 @@
 
 
 Direction Runner::step() {
-        Direction Runner::step() {
-
-//        if (current_status.up == BlockType::EXIT)
-//            return Direction::UP;
-//        if (current_status.down == BlockType::EXIT)
-//            return Direction::DOWN;
-//        if (current_status.right == BlockType::EXIT)
-//            return Direction::RIGHT;
-//        if (current_status.left == BlockType::EXIT)
-//            return Direction::LEFT;
-
+  
     if (currDirection == Direction::UP) {
         if (current_status.up == BlockType::EXIT)
             return Direction::UP;
