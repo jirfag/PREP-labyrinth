f6ad0194c1861883e72b4c6e36a19c03f1844ffd
vbpod <podvb@mail.ru>
2016-11-25 23:30:56 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -1,16 +1,62 @@
-//
-// Created by tsv on 09.05.16.
-//
-
 #include "Runner.hpp"
-#include <vector>
-#include <cstdlib>
 #include "utils.hpp"
+
+Direction direction = Direction::UP;
 
 Direction Runner::step()
 {
-    // TODO: you need to replace the code below with your implementation of labyrinth solver.
-    // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-    return directions[std::rand() % directions.size()];
+    if (current_status.up == BlockType::EXIT)
+        return Direction::UP;
+
+    if (current_status.down == BlockType::EXIT)
+        return Direction::DOWN;
+
+    if (current_status.left == BlockType::EXIT)
+        return Direction::LEFT;
+
+    if (current_status.right == BlockType::EXIT)
+        return Direction::RIGHT;
+
+    switch(direction) {
+        case Direction::UP: {
+            if (current_status.left == BlockType::FREE)
+                return direction = Direction::LEFT;
+            else if (current_status.up == BlockType::FREE)
+                return direction = Direction::UP;
+            else if (current_status.right == BlockType::FREE)
+                return direction = Direction::RIGHT;
+            else
+                return direction = Direction::DOWN;
+        }
+        case Direction::DOWN: {
+            if (current_status.right == BlockType::FREE)
+                return direction = Direction::RIGHT;
+            else if (current_status.down == BlockType::FREE)
+                return direction = Direction::DOWN;
+            else if (current_status.left == BlockType::FREE)
+                return direction = Direction::LEFT;
+            else
+                return direction = Direction::UP;
+        }
+        case Direction::LEFT: {
+            if (current_status.down == BlockType::FREE)
+                return direction = Direction::DOWN;
+            else if (current_status.left == BlockType::FREE)
+                return direction = Direction::LEFT;
+            else if (current_status.up == BlockType::FREE)
+                return direction = Direction::UP;
+            else
+                return direction = Direction::RIGHT;
+        }
+        default: {
+            if (current_status.up == BlockType::FREE)
+                return direction = Direction::UP;
+            else if (current_status.right == BlockType::FREE)
+                return direction = Direction::RIGHT;
+            else if (current_status.down == BlockType::FREE)
+                return direction = Direction::DOWN;
+            else
+                return direction = Direction::LEFT;
+        }
+    }
 }
