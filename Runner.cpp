2be6b18a17c27f747017fd6da3f65ab1ddda6c00
Андрей Кучин <andrey.kuchin97@gmail.com>
2016-11-25 20:35:38 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -127,7 +127,7 @@ Direction Runner::step() {
     if (Down == Exit)
         return god;
     a++;
-    if ((a) % 639 == 0)
+    if ((a) % 133 == 0)
         return right_hand(current_status);
     else
         return left_hand(current_status);
