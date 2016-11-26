f7dbb15ea232da09811337dee05eea9bf69440a1
Crusader727 <rubenhov97@gmail.com>
2016-11-26 13:38:06 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -1,16 +1,48 @@
-//
-// Created by tsv on 09.05.16.
-//
-
 #include "Runner.hpp"
 #include <vector>
 #include <cstdlib>
 #include "utils.hpp"
 
-Direction Runner::step()
-{
-    // TODO: you need to replace the code below with your implementation of labyrinth solver.
-    // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-    return directions[std::rand() % directions.size()];
+
+Direction Runner::step() {
+
+    if((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && (!path[foo(x+1,y)]){
+       ++x;
+      path[foo(x,y)]=1;
+      return Direction::RIGHT;
+    }
+    if((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && (!path[foo(x,y+1)]){
+      ++y;
+      path[foo(x,y)]=1;
+      return Direction::UP;
+    }
+    if((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && (!path[foo(x-1,y)]){
+      --x;
+      path[foo(x,y)]=1;
+      return Direction::LEFT;
+    }
+    if((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && (!path[foo(x,y-1)]){
+      --y;
+      path[foo(x,y)]=1;
+      return Direction::DOWN;
+    }
+    if(current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) {
+      ++x;
+      return Direction::RIGHT;
+    }
+    if(current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) {
+      ++y;
+      return Direction::UP;
+    }
+    if(current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) {
+      --x;
+      return Direction::LEFT;
+    }
+    if(current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) {
+      --y;
+      return Direction::DOWN;
+    }
+
+
+
 }
--- Runner.hpp
+++ Runner.hpp
@@ -1,15 +1,19 @@
-//
-// Created by tsv on 09.05.16.
-//
-
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
 #include "RunnerBase.hpp"
-
+#include <unordered_map>
 class Runner: public RunnerBase {
     public:
         Direction step();
+        Runner() : x(0),y(0) {
+          path[foo(0,0)]=1;
+        }
+        std::string foo(int a,int b){
+          return(to_string(a)+to_string(b));
+        }
+        int x,y;
+        std::unordered_map<std::string,bool> path;
 };
 
 
