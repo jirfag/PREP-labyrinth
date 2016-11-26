08b1e8e036fb64a06e5c8562f4039c9c275bd0d2
Kurkin Nickolay <mrnickrus@gmail.com>
2016-11-26 19:09:35 +0400

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -10,22 +10,22 @@
 
 Direction Runner::step() {
 
-    if((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && (!path[foo(x+1,y)]){
+    if((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && (!path[foo(x+1,y)])){
        ++x;
       path[foo(x,y)]=1;      
       return Direction::RIGHT;
     }
-    if((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && (!path[foo(x,y+1)]){
+    if((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && (!path[foo(x,y+1)])){
       ++y;
       path[foo(x,y)]=1; 
       return Direction::UP;
     }
-    if((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && (!path[foo(x-1,y)]){
+    if((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && (!path[foo(x-1,y)])){
       --x;
       path[foo(x,y)]=1; 
       return Direction::LEFT;
     }
-    if((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && (!path[foo(x,y-1)]){
+    if((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && (!path[foo(x,y-1)])){
       --y;
       path[foo(x,y)]=1; 
       return Direction::DOWN;
@@ -42,11 +42,6 @@ Direction Runner::step() {
       --x;
       return Direction::LEFT;
     }
-    if(current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) {
-      --y;
-      return Direction::DOWN;
-    }
-    
-   
- 
+    --y;
+    return Direction::DOWN;
 }
