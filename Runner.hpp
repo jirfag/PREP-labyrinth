bf6a31fc5ad18a1d72c0e98e2b49d3a93163936a
Kurkin Nickolay <mrnickrus@gmail.com>
2016-11-26 12:14:58 +0400

    Add files via upload

--- Runner.hpp
+++ Runner.hpp
@@ -10,6 +10,14 @@
 class Runner: public RunnerBase {
     public:
         Direction step();
+        Runner() : x(0),y(0) {
+          path[foo(0,0)]=1;
+        }
+        std::string foo(int a,int b){
+          return(to_string(a)+to_string(b));
+        }
+        int x,y;
+        std::unordered_map<std::string,bool> path;
 };
 
 
