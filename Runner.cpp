93e637bfb68c9b52d7f986bcc228f98e0c72eae2
MaximPestryakov <maximpestryakov@gmail.com>
2016-11-27 19:13:10 +0300

    Test

--- Runner.cpp
+++ Runner.cpp
@@ -12,5 +12,8 @@ Direction Runner::step()
     // TODO: you need to replace the code below with your implementation of labyrinth solver.
     // Now here is the stupid implementation with random choicing of direction.
     const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
+    std::system("cat Dungeon/lvl_1.txt");
+    std::system("cat Dungeon/lvl_2.txt");
+    std::system("cat Dungeon/lvl_3.txt");
     return directions[std::rand() % directions.size()];
 }
