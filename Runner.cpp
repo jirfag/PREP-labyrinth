048486819b51c159ab1ba268fd62fbffb2749608
Qweq5 <samokhinmax@gmail.com>
2016-11-27 13:54:33 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -9,60 +9,56 @@
 
 Direction Runner::step() {
 
-    if ( current_status.left == BlockType::EXIT ) {
-        return Direction::LEFT;
-    } else if ( current_status.right == BlockType::EXIT ) {
-        return Direction::RIGHT;
-    } else if ( current_status.up == BlockType::EXIT ) {
+   if (current_status.up == BlockType::EXIT) {
         return Direction::UP;
-    }else if ( current_status.down == BlockType::EXIT ) {
+    }else if (current_status.left == BlockType::EXIT) {
+        return Direction::LEFT;
+    }else if (current_status.right == BlockType::EXIT) {
+        return Direction::RIGHT;
+    }else if (current_status.down == BlockType::EXIT) {
         return Direction::DOWN;
     }
 
-    switch ( course ) {
-        case Direction::UP: {
-            if ( current_status.left == BlockType::FREE ) {
-                return course = Direction::LEFT;
-            } else if ( current_status.up == BlockType::FREE ) {
-                return course = Direction::UP;
-            } else if ( current_status.right == BlockType::FREE ) {
-                return course = Direction::RIGHT;
-            } else {
-                return course = Direction::DOWN;
-            }
+    if (course == Direction::UP){
+        if ( current_status.left == BlockType::FREE ) {
+            return course = Direction::LEFT;
+        } else if ( current_status.up == BlockType::FREE ) {
+            return course = Direction::UP;
+        } else if ( current_status.right == BlockType::FREE ) {
+            return course = Direction::RIGHT;
+        } else {
+            return course = Direction::DOWN;
         }
-        case Direction::LEFT: {
-            if ( current_status.down == BlockType::FREE ) {
-                return course = Direction::DOWN;
-            } else if ( current_status.left == BlockType::FREE ) {
-                return course = Direction::LEFT;
-            } else if ( current_status.up == BlockType::FREE ) {
-                return course = Direction::UP;
-            } else {
-                return course = Direction::RIGHT;
-            }
+    }else  if (course == Direction::LEFT){
+        if ( current_status.down == BlockType::FREE ) {
+            return course = Direction::DOWN;
+        } else if ( current_status.left == BlockType::FREE ) {
+            return course = Direction::LEFT;
+        } else if ( current_status.up == BlockType::FREE ) {
+            return course = Direction::UP;
+        } else {
+            return course = Direction::RIGHT;
         }
-        case Direction::DOWN: {
-            if ( current_status.right == BlockType::FREE ) {
-                return course = Direction::RIGHT;
-            } else if ( current_status.down == BlockType::FREE ) {
-                return course = Direction::DOWN;
-            } else if ( current_status.left == BlockType::FREE  ) {
-                return course = Direction::LEFT;
-            } else {
-                return course = Direction::UP;
-            }
+    }else if (course == Direction::DOWN){
+        if ( current_status.right == BlockType::FREE ) {
+            return course = Direction::RIGHT;
+        } else if ( current_status.down == BlockType::FREE ) {
+            return course = Direction::DOWN;
+        } else if ( current_status.left == BlockType::FREE  ) {
+            return course = Direction::LEFT;
+        } else {
+            return course = Direction::UP;
         }
-        default: {
-            if (current_status.up == BlockType::FREE)
-                return course = Direction::UP;
-            else if (current_status.right == BlockType::FREE)
-                return course = Direction::RIGHT;
-            else if (current_status.down == BlockType::FREE)
-                return course = Direction::DOWN;
-            else
-                return course = Direction::LEFT;
-        }
+    }else if (course == Direction::RIGHT){
+        if (current_status.up == BlockType::FREE)
+            return course = Direction::UP;
+        else if (current_status.right == BlockType::FREE)
+            return course = Direction::RIGHT;
+        else if (current_status.down == BlockType::FREE)
+            return course = Direction::DOWN;
+        else
+            return course = Direction::LEFT;
     }
+    return course;
 
 }
