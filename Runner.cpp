d424fe0c04f02b12860ad6b04fdd49cbaf1bdf74
MikeGus <vfc17@yandex.ru>
2016-11-28 01:31:57 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -5,67 +5,20 @@
 #include "Runner.hpp"
 #include <vector>
 #include <cstdlib>
+
 #include "utils.hpp"
+
+
 
 Direction Runner::step()
 {
     // TODO: you need to replace the code below with your implementation of labyrinth solver.
     // Now here is the stupid implementation with random choicing of direction.
-    //const std::vector<Direction> directions = {Direction::UP, Direction::LEFT, Direction::DOWN, Direction::RIGHT};
+    const std::vector<Direction> directions = {Direction::UP, Direction::LEFT, Direction::DOWN, Direction::RIGHT};
 
-    if (last_step == Direction::UP){
-        if(current_status.left != BlockType::WALL)
-            return last_step = Direction::LEFT;
-
-        if(current_status.up != BlockType::WALL)
-            return last_step = Direction::UP;
-
-        if(current_status.right != BlockType::WALL)
-            return last_step = Direction::RIGHT;
-
-        else
-            return last_step = Direction::DOWN;
-    }
-    else if (last_step == Direction::LEFT){
-        if(current_status.down != BlockType::WALL)
-            return last_step = Direction::DOWN;
-
-        if(current_status.left != BlockType::WALL)
-            return last_step = Direction::LEFT;
-
-        if(current_status.up != BlockType::WALL)
-            return last_step = Direction::UP;
-
-        else
-            return last_step = Direction::RIGHT;
-
-    }
-    else if (last_step == Direction::DOWN){
-        if(current_status.right != BlockType::WALL)
-            return last_step = Direction::RIGHT;
-
-        if(current_status.down != BlockType::WALL)
-            return last_step = Direction::DOWN;
-
-        if(current_status.left != BlockType::WALL)
-            return last_step = Direction::LEFT;
-
-        else
-            return last_step = Direction::UP;
-
-    }
-    else {
-        if(current_status.up != BlockType::WALL)
-            return last_step = Direction::UP;
-
-        if(current_status.right != BlockType::WALL)
-            return last_step = Direction::RIGHT;
-
-        if(current_status.down != BlockType::WALL)
-            return last_step = Direction::DOWN;
-
-        else
-            return last_step = Direction::LEFT;
-
-    }
+    return directions[rand() % 4];
+}
+void Runner::set_current_status(const BlackStatus& new_status)
+{
+    this->current_status = new_status;
 }
--- Runner.hpp
+++ Runner.hpp
@@ -6,18 +6,35 @@
 #define LABYRINTH_RUNNER_HPP
 
 #include "RunnerBase.hpp"
+enum class BlackType{
+    FREE = 0,
+    WALL = 1,
+    ENTER = 0,
+    EXIT = 0
+};
 
+struct BlackStatus {
+    BlackType up;
+    BlackType down;
+    BlackType left;
+    BlackType right;
+};
+
+#define BlockType BlackType
+#define Status BlackStatus
 class Runner: public RunnerBase {
     public:
-
         Runner(){
             last_step = Direction::UP;
         }
 
         Direction step();
 
+        void set_current_status(const BlackStatus& new_status);
+
     private:
         Direction last_step;
+        BlackStatus current_status;
 };
 
 
