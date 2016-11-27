86a8993b814a72ded8ec923f86c5deb2eb220988
Kurkin Nickolay <mrnickrus@gmail.com>
2016-11-27 19:24:19 +0400

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -7,41 +7,50 @@
 #include <cstdlib>
 #include "utils.hpp"
 
-
 Direction Runner::step() {
-
-    if((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && (!path[foo(x+1,y)])){
-       ++x;
-      path[foo(x,y)]=1;      
-      return Direction::RIGHT;
-    }
-    if((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && (!path[foo(x,y+1)])){
-      ++y;
-      path[foo(x,y)]=1; 
-      return Direction::UP;
-    }
-    if((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && (!path[foo(x-1,y)])){
-      --x;
-      path[foo(x,y)]=1; 
-      return Direction::LEFT;
-    }
-    if((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && (!path[foo(x,y-1)])){
-      --y;
-      path[foo(x,y)]=1; 
-      return Direction::DOWN;
-    }
-    if(current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) {
-      ++x;
-      return Direction::RIGHT;
-    }
-    if(current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) {
-      ++y;
-      return Direction::UP;
-    }
-    if(current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) {
-      --x;
-      return Direction::LEFT;
-    }
+  if ((current_status.right == BlockType::FREE ||
+       current_status.right == BlockType::EXIT) &&
+      (!path[foo(x + 1, y)])) {
+    ++x;
+    path[foo(x, y)] = 1;
+    return Direction::RIGHT;
+  }
+  if ((current_status.up == BlockType::FREE ||
+       current_status.up == BlockType::EXIT) &&
+      (!path[foo(x, y + 1)])) {
+    ++y;
+    path[foo(x, y)] = 1;
+    return Direction::UP;
+  }
+  if ((current_status.left == BlockType::FREE ||
+       current_status.left == BlockType::EXIT) &&
+      (!path[foo(x - 1, y)])) {
+    --x;
+    path[foo(x, y)] = 1;
+    return Direction::LEFT;
+  }
+  if ((current_status.down == BlockType::FREE ||
+       current_status.down == BlockType::EXIT) &&
+      (!path[foo(x, y - 1)])) {
     --y;
+    path[foo(x, y)] = 1;
     return Direction::DOWN;
-}
+  }
+  const std::vector<Direction> directions = {Direction::UP, Direction::DOWN,
+                                             Direction::LEFT, Direction::RIGHT};
+  Direction rand_dir = directions[std::rand() % directions.size()];
+
+  if (rand_dir == Direction::UP) {
+    ++y;
+  } 
+  if (rand_dir == Direction::DOWN) {
+    --y;
+  } 
+  if (rand_dir == Direction::RIGHT) {
+    ++x;
+  } 
+  if (rand_dir == Direction::LEFT) {
+    --x;
+  } 
+  return rand_dir;
+};
