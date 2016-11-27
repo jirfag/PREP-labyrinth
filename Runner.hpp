c217b39f94eebd2a5c0fd310ec08447def00d6da
Crusader727 <rubenhov97@gmail.com>
2016-11-27 18:27:59 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -1,8 +1,14 @@
+//
+// Created by tsv on 09.05.16.
+//
+
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
 #include "RunnerBase.hpp"
 #include <unordered_map>
+#include <string>
+
 class Runner: public RunnerBase {
     public:
         Direction step();
@@ -10,7 +16,7 @@ class Runner: public RunnerBase {
           path[foo(0,0)]=1;
         }
         std::string foo(int a,int b){
-          return(to_string(a)+to_string(b));
+          return(std::to_string(a)+";"+std::to_string(b));
         }
         int x,y;
         std::unordered_map<std::string,bool> path;
