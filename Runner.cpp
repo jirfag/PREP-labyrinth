d3ad030c26eb3cbfb2a77ef8848e5b80ba8c79f3
vbpod <podvb@mail.ru>
2016-11-27 20:44:35 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -1,10 +1,39 @@
 #include "Runner.hpp"
 #include "utils.hpp"
-
 short int direction = 0;
 
 Direction Runner::step()
 {
+    if (current_status.up == BlockType::EXIT)
+        return Direction::UP;
+
+    if (current_status.down == BlockType::EXIT)
+        return Direction::DOWN;
+
+    if (current_status.left == BlockType::EXIT)
+        return Direction::LEFT;
+
+    if (current_status.right == BlockType::EXIT)
+        return Direction::RIGHT;
+    
+    if(direction == 2) {
+            if (current_status.down == BlockType::FREE) {
+            	direction = 1;
+                return Direction::DOWN;
+            }
+            else if (current_status.left == BlockType::FREE) {
+            	direction = 2;
+                return Direction::LEFT;
+            }
+            else if (current_status.up == BlockType::FREE) {
+            	direction = 0;
+                return Direction::UP;
+            }
+            else {
+            	direction = 3;
+                return Direction::RIGHT;
+            }
+        }
 
     if(direction == 0) {
             if (current_status.left == BlockType::FREE) {
@@ -24,7 +53,8 @@ Direction Runner::step()
                 return Direction::DOWN;
             }
         }
-        if(direction == 1) {
+
+    if(direction == 1) {
             if (current_status.right == BlockType::FREE) {
             	direction = 3;
                 return Direction::RIGHT;
@@ -42,25 +72,8 @@ Direction Runner::step()
                 return Direction::UP;
             }
         }
-        if(direction == 2) {
-            if (current_status.down == BlockType::FREE) {
-            	direction = 1;
-                return Direction::DOWN;
-            }
-            else if (current_status.left == BlockType::FREE) {
-            	direction = 2;
-                return Direction::LEFT;
-            }
-            else if (current_status.up == BlockType::FREE) {
-            	direction = 0;
-                return Direction::UP;
-            }
-            else {
-            	direction = 3;
-                return Direction::RIGHT;
-            }
-        }
-        if(direction == 3) {
+
+    if(direction == 3) {
             if (current_status.up == BlockType::FREE) {
             	direction = 0;
                 return Direction::UP;
@@ -78,15 +91,4 @@ Direction Runner::step()
                 return Direction::LEFT;
             }
         }
-    if (current_status.up == BlockType::EXIT)
-        return Direction::UP;
-
-    if (current_status.down == BlockType::EXIT)
-        return Direction::DOWN;
-
-    if (current_status.left == BlockType::EXIT)
-        return Direction::LEFT;
-
-    if (current_status.right == BlockType::EXIT)
-        return Direction::RIGHT;
 }
