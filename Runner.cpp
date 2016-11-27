b47ba0b28127104d1121e3a28c0d9d9640f7ed3c
ArtemyKlimov <artemyklimov@gmail.com>
2016-11-27 20:39:07 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -7,10 +7,88 @@
 #include <cstdlib>
 #include "utils.hpp"
 
+Runner::Runner() : prevMove(Direction::RIGHT){}
+
 Direction Runner::step()
 {
-    // TODO: you need to replace the code below with your implementation of labyrinth solver.
-    // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-    return directions[std::rand() % directions.size()];
+	/*
+    if (current_status.right == BlockType::EXIT)
+        return Direction::RIGHT;
+	if(current_status.down == BlockType::EXIT)
+        return Direction::DOWN;
+    if (current_status.up == BlockType::EXIT)
+        return Direction::UP;
+    if (current_status.left == BlockType::EXIT)
+        return Direction::LEFT;
+*/
+    switch(prevMove){
+    	case Direction::RIGHT: {
+    		if(current_status.down != BlockType::WALL){
+    			prevMove = Direction::DOWN;
+    			return prevMove;
+    		}
+    		if(current_status.right !=BlockType::WALL){
+    			return prevMove;
+    		}
+    		if(current_status.up !=BlockType::WALL){
+    			prevMove = Direction::UP;
+    			return prevMove;
+    		}
+    		if(current_status.left != BlockType::WALL){
+    			prevMove = Direction::LEFT;
+    			return prevMove;
+    		}
+    	}
+    	case Direction::DOWN: {
+    		if(current_status.left != BlockType::WALL){
+    			prevMove = Direction::LEFT;
+    			return prevMove;
+    		}
+    		if(current_status.down != BlockType::WALL){
+    			return prevMove;
+    		}
+    		if(current_status.right!=BlockType::WALL){
+    			prevMove = Direction::RIGHT;
+    			return prevMove;
+    		}
+    		if(current_status.up!=BlockType::WALL){
+    			prevMove = Direction::UP;
+    			return prevMove;
+    		}
+    	}
+    	case Direction::LEFT: {
+    		if(current_status.up!=BlockType::WALL){
+    			prevMove = Direction::UP;
+    			return prevMove;
+    		}
+    		if(current_status.left != BlockType::WALL){
+    			return prevMove;
+    		}
+    		if(current_status.down != BlockType::WALL){
+    			prevMove = Direction::DOWN;
+    			return prevMove;
+    		}
+    		if(current_status.right!=BlockType::WALL){
+    			prevMove = Direction::RIGHT;
+    			return prevMove;
+    		}
+    	}
+    	case Direction::UP: {
+    		if(current_status.right!=BlockType::WALL){
+    			prevMove = Direction::RIGHT;
+    			return prevMove;
+    		}
+    		if(current_status.up!=BlockType::WALL){
+    			return prevMove;
+    		}
+    		if(current_status.left != BlockType::WALL){
+    			prevMove = Direction::LEFT;
+    			return prevMove;
+    		}
+    		if(current_status.down != BlockType::WALL){
+    			prevMove = Direction::DOWN;
+    			return prevMove;
+    		}
+    	}
+    }
 }
