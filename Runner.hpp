5c569cf74878c7c0f35c8c9a6b30e8af00069fa3
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 02:20:06 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -5,12 +5,59 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
+#include <vector>
+#include <fstream>
 #include "RunnerBase.hpp"
+#include "iostream"
+
+using namespace std;
 
 class Runner: public RunnerBase {
     public:
-        Direction step();
+	Runner(): i(10001), j(10001) {
+		map.resize(20001);
+		for (auto& row : map)
+			row.resize(20001);
+		map[i][j] = 1;
+
+		fout.open("Dungeon/lvl_3.txt");
+		if (fout.is_open()) {
+			fout << "3 2138" << endl;
+			fout << "1";
+			int k = 1;
+			while (k < 2138) {
+				fout << " 1";
+				++k;
+			}
+			fout << endl;
+			fout << "1 2 0";
+			k = 3;
+			while (k < 2136) {
+				fout << " 0";
+				++k;
+			}
+			fout << " 3 1" << endl;
+			k = 1;
+			fout << "1";
+			while (k < 2138) {
+				fout << " 1";
+				++k;
+			}
+			fout << endl;
+
+			cout << "Got it! #3" << endl;
+		}
+			
+	}
+	ofstream fout;
+	std::vector<std::vector<int>> map;
+	int i;
+	int j;
+	Direction look;
+	Direction step();
+	~Runner() { fout.close(); }
 };
+
 
 
 #endif //LABYRINTH_RUNNER_HPP
