57460103218b904911a5605bbf8d18e2b47e30a1
ArtemyKlimov <artemyklimov@gmail.com>
2016-11-28 18:58:55 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -1,87 +1,96 @@
 //
 // Created by tsv on 09.05.16.
 //
+
 #include "Runner.hpp"
 #include <vector>
+#include <cstdlib>
 #include "utils.hpp"
 
-Direction Runner::step() 
-{
-    if (v.empty()) {
-        node n;
-        v.push_back(n);
-    }
-    if (turn_back()) {
-        v.pop_back();
-        if(v.back().dir == Direction::RIGHT)
-            return Direction::LEFT;
-        if(v.back().dir == Direction::LEFT)
-            return Direction::RIGHT;
-        if(v.back().dir == Direction::UP)
-            return Direction::DOWN;
-        if(v.back().dir == Direction::DOWN)
-            return Direction::UP;
-    }
+Runner::Runner() : prevMove(Direction::RIGHT){}
 
-    if (current_status.left == BlockType::EXIT) {
-        return Direction::LEFT;
-    }
-    if (current_status.right == BlockType::EXIT) {
-        return Direction::RIGHT;
-    }
-    if (current_status.down == BlockType::EXIT) {
-        return Direction::DOWN;
-    }
+Direction Runner::step()
+{
+
+
     if (current_status.up == BlockType::EXIT) {
         return Direction::UP;
-    }
-    else{
-    if(current_status.right == BlockType::FREE && (!v.back().Right)){
-        v.back().dir = Direction::RIGHT;
-        v.back().Right = true;
-        node n;
-        n.Left = true;
-        v.push_back(n);
+    }else if (current_status.left == BlockType::EXIT) {
+        return Direction::LEFT;
+    }else if (current_status.right == BlockType::EXIT) {
         return Direction::RIGHT;
-    }
-    if (current_status.down == BlockType::FREE && (!v.back().Down)) {
-        v.back().dir = Direction::DOWN;
-        v.back().Down = true;
-        node n;
-        n.Up = true;
-        v.push_back(n);
+    }else if (current_status.down == BlockType::EXIT) {
         return Direction::DOWN;
     }
-    if (current_status.left == BlockType::FREE && (!v.back().Left)) {
-        v.back().dir = Direction::LEFT;
-        v.back().Left = true;
-        node n;
-        n.Right = true;
-        v.push_back(n);
-        return Direction::LEFT;
+    switch(prevMove){
+    	case Direction::UP: {
+    		if(current_status.left == BlockType::FREE){
+    			prevMove = Direction::LEFT;
+    			return Direction::LEFT;
+    		}
+            if(current_status.up==BlockType::FREE){
+                return Direction::UP;
+            }
+            if(current_status.right ==BlockType::FREE){
+                prevMove = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+    		if(current_status.down == BlockType::FREE){
+    			prevMove = Direction::DOWN;
+    			return Direction::DOWN;
+    		}
+        }
+        case Direction::LEFT: {
+            if(current_status.down == BlockType::FREE){
+                prevMove = Direction::DOWN;
+                return Direction::DOWN;
+            }
+            if(current_status.left == BlockType::FREE){
+                return Direction::LEFT;
+            }
+            if(current_status.up==BlockType::FREE){
+                prevMove = Direction::UP;
+                return Direction::UP;
+            }
+            if(current_status.right==BlockType::FREE){
+                prevMove = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+        }
+        case Direction::DOWN: {
+            if(current_status.right ==BlockType::FREE){
+                prevMove = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+            if(current_status.down == BlockType::FREE){
+                return Direction::DOWN;
+            }
+            if(current_status.left == BlockType::FREE){
+                prevMove = Direction::LEFT;
+                return Direction::LEFT;
+            }
+            if(current_status.up ==BlockType::FREE){
+                prevMove = Direction::UP;
+                return Direction::UP;
+            }
+        }
+        case Direction::RIGHT: {
+            if(current_status.up ==BlockType::FREE){
+                prevMove = Direction::UP;
+                return Direction::UP;
+            }
+            if(current_status.right ==BlockType::FREE){
+                return Direction::RIGHT;
+            }
+            if(current_status.down == BlockType::FREE){
+                prevMove = Direction::DOWN;
+                return Direction::DOWN;
+            }
+            if(current_status.left == BlockType::FREE){
+                prevMove = Direction::LEFT;
+                return Direction::LEFT;
+            }
+        }
     }
-    if (current_status.up == BlockType::FREE && (!v.back().Up)) {
-        v.back().dir = Direction::UP;
-        v.back().Up = true;
-        node n;
-        n.Down = true;
-        v.push_back(n);
-        return Direction::UP;        
-    }
-    }
-    return Direction::RIGHT;
-}
-
-
-bool Runner::turn_back(){
-    if ((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT ) && (!v.back().Right))
-        return false;
-    if ((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT ) && (!v.back().Down))
-        return false;
-    if ((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT ) && (!v.back().Up))
-        return false;
-    if ((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT ) && (!v.back().Left))
-        return false;
-
-    return true;
+    return prevMove;
 }
--- Runner.hpp
+++ Runner.hpp
@@ -5,26 +5,15 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
-#include <vector>
 #include "RunnerBase.hpp"
-
-struct node;
 
 class Runner: public RunnerBase {
 private:
-    std::vector<node> v;
+	Direction prevMove;
 public:
+   	Runner();
+
     Direction step();
-    bool turn_back();
-
-};
-
-struct node {
-    Direction dir;
-    bool Up = false;
-    bool Down = false;
-    bool Right = false;
-    bool Left = false;
 
 };
 
