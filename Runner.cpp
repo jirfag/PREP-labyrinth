79c9c38e9029cfcabd34b80401a00b6d3fd8e249
barzug <barsukof@live.com>
2016-11-28 21:31:37 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -83,7 +83,7 @@ Direction Runner::step() {
                 node.down += it.base()->down;
                 node.left += it.base()->left;
                 node.right += it.base()->right;
-                stack.erase( it-1, stack.end() );
+     //           stack.erase( it-1, stack.end() );
                 break;
             }
         }
