947689798ce83d4d70850726abe77690b1ec7925
ed_grolsh <Drujininii@yandex.ru>
2016-11-28 16:58:33 +0300

    commit 5

--- Runner.cpp
+++ Runner.cpp
@@ -35,22 +35,6 @@ Direction Runner::step() {
                 return Direction::RIGHT;
             }
         }
-        case Direction::DOWN: {
-            if (current_status.right == BlockType::FREE) {
-                prev_directions = Direction::RIGHT;
-                return Direction::RIGHT;
-            }
-            else if (current_status.down == BlockType::FREE) {
-                prev_directions = Direction::DOWN;
-                return Direction::DOWN;
-            } else if (current_status.left == BlockType::FREE) {
-                prev_directions = Direction::LEFT;
-                return Direction::LEFT;
-            } else {
-                prev_directions = Direction::UP;
-                return Direction::UP;
-            }
-        }
         case Direction::RIGHT: {
             if(current_status.up == BlockType::FREE) {
                 prev_directions = Direction::UP;
@@ -66,6 +50,22 @@ Direction Runner::step() {
             } else {
                 prev_directions = Direction::LEFT;
                 return Direction::LEFT;
+            }
+        }
+        case Direction::DOWN: {
+            if (current_status.right == BlockType::FREE) {
+                prev_directions = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+            else if (current_status.down == BlockType::FREE) {
+                prev_directions = Direction::DOWN;
+                return Direction::DOWN;
+            } else if (current_status.left == BlockType::FREE) {
+                prev_directions = Direction::LEFT;
+                return Direction::LEFT;
+            } else {
+                prev_directions = Direction::UP;
+                return Direction::UP;
             }
         }
         default: {
