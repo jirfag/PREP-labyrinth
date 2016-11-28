73c0f9c098f21910df14217daee978801a039539
ed_grolsh <Drujininii@yandex.ru>
2016-11-28 16:49:12 +0300

    commit 1

--- Runner.cpp
+++ Runner.cpp
@@ -20,21 +20,6 @@ Direction Runner::step() {
     }
 
     switch (prev_directions) {
-        case Direction::UP: {
-            if (current_status.left == BlockType::FREE) {
-                prev_directions = Direction::LEFT;
-                return Direction::LEFT;
-            } else if (current_status.up == BlockType::FREE) {
-                prev_directions = Direction::UP;
-                return Direction::UP;
-            } else if (current_status.right == BlockType::FREE) {
-                prev_directions = Direction::RIGHT;
-                return Direction::RIGHT;
-            } else {
-                prev_directions = Direction::DOWN;
-                return Direction::DOWN;
-            }
-        }
         case Direction::LEFT: {
             if (current_status.down == BlockType::FREE) {
                 prev_directions = Direction::DOWN;
@@ -48,6 +33,21 @@ Direction Runner::step() {
             } else {
                 prev_directions = Direction::RIGHT;
                 return Direction::RIGHT;
+            }
+        }
+        case Direction::UP: {
+            if (current_status.left == BlockType::FREE) {
+                prev_directions = Direction::LEFT;
+                return Direction::LEFT;
+            } else if (current_status.up == BlockType::FREE) {
+                prev_directions = Direction::UP;
+                return Direction::UP;
+            } else if (current_status.right == BlockType::FREE) {
+                prev_directions = Direction::RIGHT;
+                return Direction::RIGHT;
+            } else {
+                prev_directions = Direction::DOWN;
+                return Direction::DOWN;
             }
         }
         case Direction::DOWN: {
@@ -86,77 +86,4 @@ Direction Runner::step() {
     }
     return Direction::LEFT;
 }
-//        case 180: {
-//        turn_coord.up = current_status.down;
-//        turn_coord.left = current_status.right;
-//        turn_coord.down = current_status.up;
-//        turn_coord.right = current_status.left;
-//            break;
-//        }
-//    }
-//
-//
-//    switch(turn_coord.left) {
-//        case BlockType::WALL: {
-//            if (turn_coord.up == BlockType::FREE) {
-//                if (angle == 0)
-//                    return Direction::UP;
-//                else if (angle == 90)
-//                    return Direction::LEFT;
-//                else if (angle == -90)
-//                    return Direction::RIGHT;
-//                else
-//                    return Direction::DOWN;
-//            } else if (turn_coord.up == BlockType::WALL && turn_coord.right == BlockType::FREE) {
-//                angle = (angle + -90);
-//                if (angle <= 180 && angle >= -90);
-//                else
-//                    angle = angle + (angle > 180 ? (-360) : (360));
-//                if (angle == 0)
-//                    return Direction::UP;
-//                else if (angle == 90)
-//                    return Direction::LEFT;
-//                else if (angle == -90)
-//                    return Direction::RIGHT;
-//                else
-//                    return Direction::DOWN;
-//            }
-//            else
-//                angle = (angle + 180);
-//            if (angle <= 180 && angle >= -90);
-//            else
-//                angle = angle + (angle > 180 ? (-360) : (360));
-//            if (angle == 0)
-//                return Direction::UP;
-//            else if (angle == 90)
-//                return Direction::LEFT;
-//            else if (angle == -90)
-//                return Direction::RIGHT;
-//            else
-//                return Direction::DOWN;
-//
-//        }
-//        case BlockType::FREE: {
-//            angle = (angle + 90);
-//            if (angle <= 180 && angle >= -90);
-//            else
-//                angle = angle + (angle > 180 ? (-360) : (360));
-//            if (angle == 0)
-//                return Direction::UP;
-//            else if (angle == 90)
-//                return Direction::LEFT;
-//            else if (angle == -90)
-//                return Direction::RIGHT;
-//            else
-//                return Direction::DOWN;
-//        }
-//        case BlockType::ENTER: {
-//            break;
-//        }
-//        case BlockType::EXIT: {
-//            break;
-////
-//        }
-//    }
-//
-//            return Direction::DOWN;
+
