03a8d334dd4d1682613f28c9efd885fa8fd57ac5
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-28 22:42:28 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -12,19 +12,6 @@ using namespace std;
 
 class Runner: public RunnerBase {
     public:
-	Runner(): i(10000), j(10000),
-		  prev(Direction::UP), deadend(true) {
-		map.resize(20001);
-		for (auto& row : map)
-			row.resize(20001);
-		map[i][j] = 1;
-	}
-
-	std::vector<std::vector<int>> map;
-	int i;
-	int j;
-	Direction prev;
-	bool deadend;
         Direction step();
 
 };
