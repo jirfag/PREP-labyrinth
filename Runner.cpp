ff1bd586a0b882b0bef76c7d5bec2d20dcd638ad
dondiego4697 <dondiego4697@mail.ru>
2016-11-28 14:11:27 +0400

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -6,11 +6,68 @@
 #include <vector>
 #include <cstdlib>
 #include "utils.hpp"
+#include <iostream>
 
-Direction Runner::step()
-{
-    // TODO: you need to replace the code below with your implementation of labyrinth solver.
-    // Now here is the stupid implementation with random choicing of direction.
-    const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-    return directions[std::rand() % directions.size()];
-}
+Direction Runner::step() {
+
+  if (current_status.right == BlockType::EXIT) {
+    ++x;
+    path[foo(x, y)] = 1;
+    return Direction::RIGHT;
+  }
+   if (current_status.up == BlockType::EXIT) {
+    ++y;
+    path[foo(x, y)] = 1;
+    return Direction::UP;
+  }
+  if (current_status.left == BlockType::EXIT) {
+    --x;
+    path[foo(x, y)] = 1;
+    return Direction::LEFT;
+  }
+   if (current_status.down == BlockType::EXIT) {
+    --y;
+    path[foo(x, y)] = 1;
+    return Direction::DOWN;
+  }
+  if ( (current_status.right == BlockType::FREE) && (!path[foo(x + 1, y)]) ) {
+    ++x;
+    path[foo(x, y)] = 1;
+    order.push(Direction::LEFT);
+    return Direction::RIGHT;
+  }
+  if ( (current_status.up == BlockType::FREE) && (!path[foo(x, y + 1)]) ) {
+    ++y;
+    path[foo(x, y)] = 1;
+    order.push(Direction::DOWN);
+    return Direction::UP;
+  }
+  if ( (current_status.left == BlockType::FREE) && (!path[foo(x - 1, y)]) ) {
+    --x;
+    path[foo(x, y)] = 1;
+    order.push(Direction::RIGHT);
+    return Direction::LEFT;
+  }
+  if ( (current_status.down == BlockType::FREE) && (!path[foo(x, y - 1)]) ) {
+    --y;
+    path[foo(x, y)] = 1;
+    order.push(Direction::UP);
+    return Direction::DOWN;
+  }
+  prev=order.top();
+  order.pop();
+  if (prev==Direction::UP) {
+    ++y;
+    return prev;
+  }
+  if (prev==Direction::DOWN) {
+    --y;
+    return prev;
+  }
+  if (prev==Direction::RIGHT) {
+    ++x;
+    return prev;
+  }
+  --x;
+  return prev;
+};
--- Runner.hpp
+++ Runner.hpp
@@ -6,10 +6,23 @@
 #define LABYRINTH_RUNNER_HPP
 
 #include "RunnerBase.hpp"
+#include <unordered_map>
+#include <string>
+#include <stack>
 
 class Runner: public RunnerBase {
     public:
         Direction step();
+        Runner() : x(0),y(0) {
+          path[foo(0,0)]=1;
+        }
+        std::string foo(int a,int b){
+          return(std::to_string(a)+";"+std::to_string(b));
+        }
+        int x,y;
+        Direction prev;
+        std::unordered_map<std::string,bool> path;
+        std::stack<Direction> order;
 };
 
 
