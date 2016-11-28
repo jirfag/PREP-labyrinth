dcf25071c5102ddbf1a1834ca3c10824ca3593eb
zwirec <zwirec@icloud.com>
2016-11-28 13:29:53 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -9,14 +9,12 @@ Direction Runner::step() {
                 return Direction::UP;
             if (current_status.right == BlockType::EXIT)
                 return Direction::RIGHT;
-            if (current_status.left == BlockType::EXIT)
-                return Direction::LEFT;
 
-            if (current_status.left == BlockType::FREE)
+            if (current_status.left != BlockType::WALL)
                 return currDirection = 1, Direction::LEFT;
-            if (current_status.up == BlockType::FREE)
+            if (current_status.up != BlockType::WALL)
                 return Direction::UP;
-            if (current_status.right == BlockType::FREE)
+            if (current_status.right != BlockType::WALL)
                 return currDirection = 3, Direction::RIGHT;
 
             return currDirection = 2, Direction::DOWN;
@@ -24,16 +22,14 @@ Direction Runner::step() {
         if (currDirection == 2) {
             if (current_status.down == BlockType::EXIT)
                 return Direction::DOWN;
-            if (current_status.right == BlockType::EXIT)
-                return Direction::RIGHT;
             if (current_status.left == BlockType::EXIT)
                 return Direction::LEFT;
 
-            if (current_status.right == BlockType::FREE)
+            if (current_status.right != BlockType::WALL)
                 return currDirection = 3, Direction::RIGHT;
-            if (current_status.down == BlockType::FREE)
+            if (current_status.down != BlockType::WALL)
                 return Direction::DOWN;
-            if (current_status.left == BlockType::FREE)
+            if (current_status.left != BlockType::WALL)
                 return currDirection = 1, Direction::LEFT;
 
             return currDirection = 0, Direction::UP;
@@ -44,32 +40,28 @@ Direction Runner::step() {
                 return Direction::UP;
             if (current_status.left == BlockType::EXIT)
                 return Direction::LEFT;
-            if (current_status.down == BlockType::EXIT)
-                return Direction::DOWN;
 
-            if (current_status.down == BlockType::FREE)
+            if (current_status.down != BlockType::WALL)
                 return currDirection = 2, Direction::DOWN;
-            if (current_status.left == BlockType::FREE)
+            if (current_status.left != BlockType::WALL)
                 return Direction::LEFT;
-            if (current_status.up == BlockType::FREE)
+            if (current_status.up != BlockType::WALL)
                 return currDirection = 0, Direction::UP;
 
             return currDirection = 3, Direction::RIGHT;
         }
-        if (current_status.up == BlockType::EXIT)
-            return Direction::UP;
         if (current_status.right == BlockType::EXIT)
             return Direction::RIGHT;
         if (current_status.down == BlockType::EXIT)
             return Direction::DOWN;
 
-        if (current_status.up == BlockType::FREE)
+        if (current_status.up != BlockType:WALL)
             return currDirection = 0, Direction::UP;
-        if (current_status.right == BlockType::FREE)
+        if (current_status.right != BlockType::WALL)
             return Direction::RIGHT;
-        if (current_status.down == BlockType::FREE)
+        if (current_status.down != BlockType::WALL)
             return currDirection = 2, Direction::DOWN;
-        if (current_status.left == BlockType::FREE)
+        if (current_status.left != BlockType::WALL)
             return currDirection = 1, Direction::LEFT;
    
     return Direction::LEFT;
