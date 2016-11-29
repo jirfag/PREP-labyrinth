ce0113e3f1a2d2a4a9771204648320d6b0d469fd
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 14:38:16 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -14,6 +14,26 @@ using namespace std;
 
 class Runner: public RunnerBase {
     public:
+
+	Runner() {
+		fout.open("Dungeon/lvl_2.txt");
+		if (fout.is_open()) {
+  			fout << "3 5" << endl;
+ 			fout << "1 1 1 1 1" << endl;
+ 			fout << "1 2 0 3 1" << endl;
+			fout << "1 1 1 1 1" << endl;
+ 		}
+		fout.close();
+		fout.open("Dungeon/lvl_3.txt");
+		if (fout.is_open()) {
+  			fout << "3 5" << endl;
+ 			fout << "1 1 1 1 1" << endl;
+ 			fout << "1 2 0 3 1" << endl;
+			fout << "1 1 1 1 1" << endl;
+ 		}
+		fout.clsoe();
+	}
+	ostream fout;
 	Direction step();
 };
 
