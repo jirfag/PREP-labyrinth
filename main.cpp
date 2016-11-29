74cd2814732234a526fa20aa7b4b57d1e5003b08
Андрей Кучин <andrey.kuchin97@gmail.com>
2016-11-29 15:29:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,9 +28,6 @@ int main(int argc, char* argv[])
             if (!field.tic())
                 return 1;
         }
-        Field a;
-        a.start();
-        field = a;
         field.result(cout);
 
     }
