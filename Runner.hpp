fee7253b162a9a4b84d6f8d3d70edc9534f1ca7b
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 03:15:49 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -14,39 +14,7 @@ using namespace std;
 
 class Runner: public RunnerBase {
     public:
-	Runner() {
-		fout.open("Dungeon/lvl_3.txt");
-		if (fout.is_open()) {
-			fout << "3 100" << endl;
-			fout << "1";
-			int k = 1;
-			while (k < 100) {
-				fout << " 1";
-				++k;
-			}
-			fout << endl;
-			fout << "1 2 0";
-			k = 3;
-			while (k < 98) {
-				fout << " 0";
-				++k;
-			}
-			fout << " 3 1" << endl;
-			k = 1;
-			fout << "1";
-			while (k < 100) {
-				fout << " 1";
-				++k;
-			}
-			fout << endl;
-
-			cout << "Got it! #3" << endl;
-		}
-			
-	}
-	ofstream fout;
 	Direction step();
-	~Runner() { fout.close(); }
 };
 
 
