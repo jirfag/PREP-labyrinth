01e052b7b04f963cfc84388b4bb7d9c349d8d3a8
zwirec <zwirec@icloud.com>
2016-11-29 20:58:22 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -2,15 +2,65 @@
 #define LABYRINTH_RUNNER_HPP
 
 
-#include "RunnerBase.hpp"
+#include <stack>
+#include <list>
+#include <algorithm>
 
+#include "RunnerBase.hpp"
+class Cell;
 
 class Runner: public RunnerBase {
 public:
-    Runner() : currDirection(3){};
+    // Constructors
+    Runner();
+
     Direction step();
+
 private:
-   short int currDirection;
+    std::stack<Cell> history;
+    std::list<std::pair<int, int>> deadlocks;
+
+    Direction lastChoice;
+    bool isForwardDirection;
+
+    int x;
+    int y;
+
+    bool checkForDeadlock(int x, int y) const;
+    bool checkForDeadlock(const Direction&) const;
+
+    void addDeadlock(int x, int y);
 };
+
+
+class Cell{
+public:
+    Cell();
+    Cell(const Status& state, const Direction& prevStep);
+    Cell(const Status& state);
+
+    bool isDeadlock() const;
+    Direction getBackDirection() const;
+
+    Direction chooseNextDirection() const;
+
+    bool getDirectionState(const Direction&) const;
+    void setDirectionState(const Direction&, bool value);
+
+private:
+    bool leftDone;
+    bool rightDone;
+    bool upDone;
+    bool downDone;
+
+    Status state;
+
+    bool isStart;
+
+    Direction prevStep;
+    Direction backDirection;
+};
+
+Direction getOppositeDirection(const Direction& direction);
 
 #endif //LABYRINTH_RUNNER_HPP
