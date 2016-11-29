fbe25ab08d45014f52cfef63055f741bdd268275
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 16:36:08 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -5,35 +5,14 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
-#include <vector>
-#include <fstream>
 #include "RunnerBase.hpp"
-#include "iostream"
 
 using namespace std;
 
 class Runner: public RunnerBase {
-    public:
-
-	Runner() {
-		fout.open("Dungeon/lvl_2.txt");
-		if (fout.is_open()) {
-  			fout << "3 5" << endl;
- 			fout << "1 1 1 1 1" << endl;
- 			fout << "1 2 0 3 1" << endl;
-			fout << "1 1 1 1 1" << endl;
- 		}
-		fout.close();
-		fout.open("Dungeon/lvl_3.txt");
-		if (fout.is_open()) {
-  			fout << "3 5" << endl;
- 			fout << "1 1 1 1 1" << endl;
- 			fout << "1 2 0 3 1" << endl;
-			fout << "1 1 1 1 1" << endl;
- 		}
-		fout.clsoe();
-	}
-	ostream fout;
+	public:
+	Runner() { prev = Direction::UP; }
+	Direction prev;
 	Direction step();
 };
 
