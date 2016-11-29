cc7d3ed88c5135d62d31720c06eec0990a15f825
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 20:47:07 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -20,82 +20,142 @@ using namespace std;
     return Direction::UP;
 	
 	++map[i][j];
-	if (map[i][j] > 5) {
-		const std::vector<Direction> directions = {Direction::UP, Direction::DOWN, Direction::LEFT, Direction::RIGHT};
-        return directions[std::rand() % directions.size()];
-	}
 
     switch (prev) {
 
     case(Direction::UP):
     if (current_status.left != BlockType::WALL) {
-    --j;
-    return (prev = Direction::LEFT);
+        --j;
+        if (map[i][j] > 2) {
+            ++j;
+            continue;
+        }
+        return (prev = Direction::LEFT);
     }
     if (current_status.up != BlockType::WALL) {
-    --i;
-    return (prev = Direction::UP);
+        --i;
+        if (map[i][j] > 2) {
+            ++i;
+            continue;
+        }
+        return (prev = Direction::UP);
     }
     if (current_status.right != BlockType::WALL) {
-    ++j;
-    return (prev = Direction::RIGHT);
+        ++j;
+        if (map[i][j] > 2) {
+            --j;
+            continue;
+        }
+        return (prev = Direction::RIGHT);
     }
     if (current_status.down != BlockType::WALL) {
     ++i;
+        if (map[i][j] > 2) {
+            --i;
+            continue;
+        }
     return (prev = Direction::DOWN);
     }
 
     case(Direction::DOWN):
     if (current_status.right != BlockType::WALL) {
     ++j;
+        if (map[i][j] > 2) {
+            --j;
+            continue;
+        }
     return (prev = Direction::RIGHT);
     }
     if (current_status.down != BlockType::WALL) {
     ++i;
+        if (map[i][j] > 2) {
+            --i;
+            continue;
+        }
     return (prev = Direction::DOWN);
     }
     if (current_status.left != BlockType::WALL) {
     --j;
+        if (map[i][j] > 2) {
+            ++j;
+            continue;
+        }
     return (prev = Direction::LEFT);
     }
     if (current_status.up != BlockType::WALL) {
     --i;
+        if (map[i][j] > 2) {
+            ++i;
+            continue;
+        }
     return (prev = Direction::UP);
     }
 
     case(Direction::RIGHT):
     if (current_status.up != BlockType::WALL) {
     --i;
+        if (map[i][j] > 2) {
+            ++i;
+            continue;
+        }
     return (prev = Direction::UP);
     }
     if (current_status.right != BlockType::WALL) {
     ++j;
+        if (map[i][j] > 2) {
+            --j;
+            continue;
+        }
     return (prev = Direction::RIGHT);
     }
     if (current_status.down != BlockType::WALL) {
     ++i;
+        if (map[i][j] > 2) {
+            --i;
+            continue;
+        }
     return (prev = Direction::DOWN);
     }
     if (current_status.left != BlockType::WALL) {
     --j;
+        if (map[i][j] > 2) {
+            ++j;
+            continue;
+        }
     return (prev = Direction::LEFT);
     }
 
     case(Direction::LEFT):
     if (current_status.down != BlockType::WALL) {
     ++i;
+        if (map[i][j] > 2) {
+            --i;
+            continue;
+        }
     return (prev = Direction::DOWN);
     }
     if (current_status.left != BlockType::WALL) {
     --j;
+        if (map[i][j] > 2) {
+            ++j;
+            continue;
+        }
     return (prev = Direction::LEFT);
     }
     if (current_status.up != BlockType::WALL) {
     --i;
+        if (map[i][j] > 2) {
+            ++i;
+            continue;
+        }
     return (prev = Direction::UP);
     }
     if (current_status.right != BlockType::WALL) {
     ++j;
+        if (map[i][j] > 2) {
+            --j;
+            continue;
+        }
     return (prev = Direction::RIGHT);
     }
     }
