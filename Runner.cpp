78ce512accacdb2724908885f45b797985e7cd10
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 16:37:31 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -6,7 +6,77 @@
 #include <cstdlib>
 #include "utils.hpp"
 
+	
+using namespace std;
+
 Direction Runner::step() {
 	
-	return Direction::RIGHT;
+    if (current_status.down == BlockType::EXIT)
+        return Direction::DOWN;
+    if (current_status.right == BlockType::EXIT)
+        return Direction::RIGHT;
+    if (current_status.left == BlockType::EXIT)
+        return Direction::LEFT;
+	if (current_status.up == BlockType::EXIT)
+			return Direction::UP;
+
+    switch (prev) {
+
+        case(Direction::UP):
+            if (current_status.left != BlockType::WALL) {
+                return (prev = Direction::LEFT);
+            }
+            if (current_status.up != BlockType::WALL) {
+                return (prev = Direction::UP);
+            }
+            if (current_status.right != BlockType::WALL) {
+                return (prev = Direction::RIGHT);
+            }
+            if (current_status.down != BlockType::WALL) {
+                return (prev = Direction::DOWN);
+            }
+
+        case(Direction::DOWN):
+            if (current_status.right != BlockType::WALL) {
+                return (prev = Direction::RIGHT);
+            }
+            if (current_status.down != BlockType::WALL) {
+                return (prev = Direction::DOWN);
+            }
+            if (current_status.left != BlockType::WALL) {
+                return (prev = Direction::LEFT);
+            }
+            if (current_status.up != BlockType::WALL) {
+                return (prev = Direction::UP);
+            }
+
+        case(Direction::RIGHT):
+            if (current_status.up != BlockType::WALL) {
+                return (prev = Direction::UP);
+            }
+            if (current_status.right != BlockType::WALL) {
+                return (prev = Direction::RIGHT);
+            }
+            if (current_status.down != BlockType::WALL) {
+                 return (prev = Direction::DOWN);
+            }
+            if (current_status.left != BlockType::WALL) {
+                return (prev = Direction::LEFT);
+            }
+
+        case(Direction::LEFT):
+            if (current_status.down != BlockType::WALL) {
+                return (prev = Direction::DOWN);
+            }
+            if (current_status.left != BlockType::WALL) {
+                return (prev = Direction::LEFT);
+            }
+            if (current_status.up != BlockType::WALL) {
+                return (prev = Direction::UP);
+            }
+            if (current_status.right != BlockType::WALL) {
+                return (prev = Direction::RIGHT);
+            }
+    }
+    return Direction::DOWN;
 }
