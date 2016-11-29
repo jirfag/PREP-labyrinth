f10d3b3e2a557128e238fb801614f5f381c4f270
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-29 19:12:47 +0300

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -5,16 +5,28 @@
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
+#include <vector>
+#include <fstream>
 #include "RunnerBase.hpp"
+#include "iostream"
 
 using namespace std;
 
 class Runner: public RunnerBase {
-	public:
-//	Runner() { prev = Direction::UP; }
-//	Direction prev;
+    public:
+	Runner(): prev(Direction::UP), i(10001), j(100001) {
+			map.resize(20001);
+			for (auto& row: map)
+				row.resize(20001);
+			map[i][j] = 0;
+		 }
+	vector<vector<int>> map;
+	Direction prev;
+	int i;
+	int j;
 	Direction step();
 };
+
 
 
 
