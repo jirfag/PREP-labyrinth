a2e363994813803c83395e06dcaf0d60efe02743
barzug <barsukof@live.com>
2016-11-29 12:11:38 +0300

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -257,7 +257,40 @@ Direction Runner::step() {
             return Direction::UP;
 
         }
+        if ( node.right == 3 && current_status.right == BlockType::FREE ) {
+            ++node.right;
+            node.direction = Direction::RIGHT;
+            stack.push_back( node );
+            ++X;
+            return Direction::RIGHT;
+        }
+
+        if ( node.down == 3 && current_status.down == BlockType::FREE ) {
+            ++node.down;
+            node.direction = Direction::DOWN;
+            stack.push_back( node );
+            --Y;
+            return Direction::DOWN;
+
+        }
+
+        if ( node.left == 3 && current_status.left == BlockType::FREE ) {
+            ++node.left;
+            node.direction = Direction::LEFT;
+            stack.push_back( node );
+            --X;
+            return Direction::LEFT;
+        }
+
+        if ( node.up == 3 && current_status.up == BlockType::FREE ) {
+            ++node.up;
+            node.direction = Direction::UP;
+            stack.push_back( node );
+            ++Y;
+            return Direction::UP;
+
+        }
         return Direction::UP;
     }
-    
 }
+
