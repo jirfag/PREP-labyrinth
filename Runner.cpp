4d61eabd3712f7e70e0c82a34b2d908298dedaa0
ed_grolsh <Drujininii@yandex.ru>
2016-11-29 03:36:09 +0300

    commit 19

--- Runner.cpp
+++ Runner.cpp
@@ -5,7 +5,7 @@
 #include "Runner.hpp"
 
 Direction Runner::step() {
-    if(count == 10070712) {
+    if(count == 10070710) {
         if (current_status.left == BlockType::EXIT) {
             return Direction::LEFT;
         } else if (current_status.right == BlockType::EXIT) {
@@ -33,6 +33,21 @@ Direction Runner::step() {
                 return Direction::DOWN;
             }
         }
+        case Direction::LEFT: {
+            if (current_status.down == BlockType::FREE) {
+                prev_directions = Direction::DOWN;
+                return Direction::DOWN;
+            } else if (current_status.left == BlockType::FREE) {
+                prev_directions = Direction::LEFT;
+                return Direction::LEFT;
+            } else if (current_status.up == BlockType::FREE) {
+                prev_directions = Direction::UP;
+                return Direction::UP;
+            } else {
+                prev_directions = Direction::RIGHT;
+                return Direction::RIGHT;
+            }
+        }
         case Direction::DOWN:  {
             if (current_status.right == BlockType::FREE) {
                 prev_directions = Direction::RIGHT;
@@ -47,21 +62,6 @@ Direction Runner::step() {
             } else {
                 prev_directions = Direction::UP;
                 return Direction::UP;
-            }
-        }
-        case Direction::LEFT: {
-            if (current_status.down == BlockType::FREE) {
-                prev_directions = Direction::DOWN;
-                return Direction::DOWN;
-            } else if (current_status.left == BlockType::FREE) {
-                prev_directions = Direction::LEFT;
-                return Direction::LEFT;
-            } else if (current_status.up == BlockType::FREE) {
-                prev_directions = Direction::UP;
-                return Direction::UP;
-            } else {
-                prev_directions = Direction::RIGHT;
-                return Direction::RIGHT;
             }
         }
         default: {
