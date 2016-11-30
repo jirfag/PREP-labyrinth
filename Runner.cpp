62b83172678485ffa916c8da12811b856761e300
Kurkin Nickolay <mrnickrus@gmail.com>
2016-11-30 22:59:25 +0300

    Add files via upload

--- Runner.cpp
+++ Runner.cpp
@@ -1,110 +1,113 @@
 #include "Runner.hpp"
-#include "utils.hpp"
 
-#include <iostream>
-
-bool Point::operator==(const Point& point) const {
-  return this->x == point.x && this->y == point.y;
+Cell :: Cell()
+{
+    left = SAFE;
+    right = SAFE;
+    up = SAFE;
+    down = SAFE;
+}
+Cell :: Cell(const Cell& c)
+{
+	left = c.left;
+	right = c.right;
+	up = c.up;
+	down = c.down;
 }
 
-std::size_t PointHash::operator()(const Point& point) const {
-  return hasher_(point.x) ^ hasher_(point.y);
+Direction Runner::step()
+{
+    if(!path.size())
+    {
+        Cell c;
+        path.push(c);
+    }
+
+	if ((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && path.top().up == SAFE)
+	{
+		Cell c1 = path.top();
+		Cell c2;
+		c1.up = FROM;
+		c2.down = FROM;
+		path.pop();
+		path.push(c1);
+		path.push(c2);
+		return Direction::UP;
+	}
+	if ((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && path.top().left == SAFE)
+	{
+		Cell c1 = path.top();
+		Cell c2;
+		c1.left = FROM;
+		c2.right = FROM;
+		path.pop();
+		path.push(c1);
+		path.push(c2);
+		return Direction::LEFT;
+	}
+	if ((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && path.top().down == SAFE)
+	{
+		Cell c1 = path.top();
+		Cell c2;
+		c1.down = FROM;
+		c2.up = FROM;
+		path.pop();
+		path.push(c1);
+		path.push(c2);
+		return Direction::DOWN;
+	}
+	if ((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && path.top().right == SAFE)
+	{
+		Cell c1 = path.top();
+		Cell c2;
+		c1.right = FROM;
+		c2.left = FROM;
+		path.pop();
+		path.push(c1);
+		path.push(c2);
+		return Direction::RIGHT;
+	}
+
+
+
+	if ((current_status.up == BlockType::FREE || current_status.up == BlockType::EXIT) && path.top().up != WALL)
+	{
+		path.pop();
+		Cell c1 = path.top();
+		c1.down = WALL;
+		path.pop();
+		path.push(c1);
+		return Direction::UP;
+	}
+	if ((current_status.left == BlockType::FREE || current_status.left == BlockType::EXIT) && path.top().left != WALL)
+	{
+		path.pop();
+		Cell c1 = path.top();
+		c1.right = WALL;
+		path.pop();
+		path.push(c1);
+		return Direction::LEFT;
+	}
+	if ((current_status.down == BlockType::FREE || current_status.down == BlockType::EXIT) && path.top().down != WALL)
+	{
+		path.pop();
+		Cell c1 = path.top();
+		c1.up = WALL;
+		path.pop();
+		path.push(c1);
+		return Direction::DOWN;
+	}
+	if ((current_status.right == BlockType::FREE || current_status.right == BlockType::EXIT) && path.top().right != WALL)
+	{
+		path.pop();
+		Cell c1 = path.top();
+		c1.left = WALL;
+		path.pop();
+		path.push(c1);
+		return Direction::RIGHT;
+	}
+
+
+
+
 }
-
-Runner::Runner() : position_({0, 0}) { visited_.insert(position_); };
-
-bool Runner::wasHere(Direction direction) const {
-  switch (direction) {
-    case Direction::DOWN:
-      return visited_.find({position_.x, position_.y - 1}) != visited_.end();
-
-    case Direction::LEFT:
-      return visited_.find({position_.x - 1, position_.y}) != visited_.end();
-
-    case Direction::RIGHT:
-      return visited_.find({position_.x + 1, position_.y}) != visited_.end();
-
-    case Direction::UP:
-      return visited_.find({position_.x, position_.y + 1}) != visited_.end();
-  }
-  return false;
-}
-
-Direction Runner::goBack() {
-  Direction last = path_.top();
-  path_.pop();
-  switch (last) {
-    case Direction::DOWN:
-      return goUp(false);
-
-    case Direction::LEFT:
-      return goRight(false);
-
-    case Direction::RIGHT:
-      return goLeft(false);
-
-    case Direction::UP:
-    default:
-      return goDown(false);
-  }
-}
-
-Direction Runner::goDown(bool savePath = true) {
-  if (savePath) {
-    path_.push(Direction::DOWN);
-  }
-  position_.y -= 1;
-  visited_.insert(position_);
-  return Direction::DOWN;
-}
-
-Direction Runner::goLeft(bool savePath = true) {
-  if (savePath) {
-    path_.push(Direction::LEFT);
-  }
-  position_.x -= 1;
-  visited_.insert(position_);
-  return Direction::LEFT;
-}
-
-Direction Runner::goRight(bool savePath = true) {
-  if (savePath) {
-    path_.push(Direction::RIGHT);
-  }
-  position_.x += 1;
-  visited_.insert(position_);
-  return Direction::RIGHT;
-}
-
-Direction Runner::goUp(bool savePath = true) {
-  if (savePath) {
-    path_.push(Direction::UP);
-  }
-  position_.y += 1;
-  visited_.insert(position_);
-  return Direction::UP;
-}
-
-Direction Runner::step() {
-  if (current_status.right == BlockType::EXIT ||
-      (current_status.right == BlockType::FREE && !wasHere(Direction::RIGHT))) {
-    return goRight();
-  }
-
-  if (current_status.up == BlockType::EXIT ||
-      (current_status.up == BlockType::FREE && !wasHere(Direction::UP))) {
-    return goUp();
-  }
-
-  if (current_status.left == BlockType::EXIT ||
-      (current_status.left == BlockType::FREE && !wasHere(Direction::LEFT))) {
-    return goLeft();
-  }
-
-  if (current_status.down == BlockType::EXIT ||
-      (current_status.down == BlockType::FREE && !wasHere(Direction::DOWN))) {
-    return goDown();
-  }
-
-  return goBack();
-};
--- Runner.hpp
+++ Runner.hpp
@@ -1,52 +1,42 @@
+//
+// Created by tsv on 09.05.16.
+//
+
 #ifndef LABYRINTH_RUNNER_HPP
 #define LABYRINTH_RUNNER_HPP
 
 #include "RunnerBase.hpp"
-#include "utils.hpp"
-
 #include <stack>
-#include <unordered_set>
 
-struct Point {
-  int x;
-  int y;
 
-  bool operator==(const Point&) const;
+
+using namespace std;
+
+
+static const int SAFE = 0;
+static const int WALL = 3315;
+static const int FROM = 999999;
+
+class Cell
+{
+public:
+	int left = SAFE;
+	int right = SAFE;
+	int up = SAFE;
+	int down = SAFE;
+
+    Cell();
+	Cell(const Cell& c);
 };
 
-class PointHash {
- public:
-  std::size_t operator()(const Point&) const;
 
- private:
-  std::hash<int> hasher_;
-};
-
-using Path = std::stack<Direction>;
-using PointSet = std::unordered_set<Point, PointHash>;
 
 class Runner : public RunnerBase {
- public:
-  Runner();
-
-  Direction step();
-
- private:
-  Path path_;
-  Point position_;
-  PointSet visited_;
-
-  bool wasHere(Direction) const;
-
-  Direction goBack();
-
-  Direction goDown(bool);
-
-  Direction goLeft(bool);
-
-  Direction goRight(bool);
-
-  Direction goUp(bool);
+public:
+	std::stack<Cell> path;
+	int rebellion;
+	bool flag;
+    Direction step();
 };
 
-#endif
+#endif //LABYRINTH_RUNNER_HPP
