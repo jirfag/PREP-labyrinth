ee22d0959aad1ade6b8d9e03072e58e7ac417004
AndersRichter <andreysav96@rambler.ru>
2016-11-30 05:48:26 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -223,99 +223,167 @@ if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_stat
 	
 	
 	
-		switch (napravlenie)
+	switch (napravlenie)
+	{
+		case 1: 
 		{
-			case R: 
+			if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))  
 			{
-				if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))  
+				if (current_status.right == BlockType::EXIT)
 				{
-					napravlenie = U;
-					return Direction::UP;
-				}
-				else if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
-				{
-					napravlenie = R;
 					return Direction::RIGHT;
 				}
-				else if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
+				else if (current_status.down == BlockType::EXIT)
 				{
-					napravlenie = D;
 					return Direction::DOWN;
 				}
 				else
 				{
-					napravlenie = L;
-					return Direction::LEFT;
-				}
-			}
-			case D: 
-			{
-				if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
-				{
-					napravlenie = R;
-					return Direction::RIGHT;
-				}
-				else if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
-				{
-					napravlenie = D;
-					return Direction::DOWN;
-				}
-				else if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
-				{
-					napravlenie = L;
-					return Direction::LEFT;
-				}
-				else
-				{
-					napravlenie = U;
+					napravlenie = 4;
 					return Direction::UP;
 				}
 			}
-			case L: 
+			else if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
 			{
-				if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
+				if (current_status.down == BlockType::EXIT)
 				{
-					napravlenie = D;
 					return Direction::DOWN;
-				}
-				else if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
-				{
-					napravlenie = L;
-					return Direction::LEFT;
-				}
-				else if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))
-				{
-					napravlenie = U;
-					return Direction::UP;
 				}
 				else
 				{
-					napravlenie = R;
 					return Direction::RIGHT;
 				}
 			}
-			default: 
+			else if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
 			{
-				if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
-				{
-					napravlenie = L;
-					return Direction::LEFT;
-				}
-				else if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))
-				{
-					napravlenie = U;
-					return Direction::UP;
-				}
-				else if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
-				{
-					napravlenie = R;
-					return Direction::RIGHT;
-				}
-				else
-				{
-					napravlenie = D;
-					return Direction::DOWN;
-				}
+				napravlenie = 2;
+				return Direction::DOWN;
+			}
+			else
+			{
+				napravlenie = 3;
+				return Direction::LEFT;
 			}
 		}
+		case 2: 
+		{
+			if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
+			{
+				if (current_status.down == BlockType::EXIT)
+				{
+					return Direction::DOWN;
+				}
+				else if (current_status.left == BlockType::EXIT)
+				{
+					return Direction::LEFT;
+				}
+				else
+				{	
+					napravlenie = 1;
+					return Direction::RIGHT;
+				}
+			}
+			else if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
+			{
+				if (current_status.left == BlockType::EXIT)
+				{
+					return Direction::LEFT;
+				}
+				else
+				{
+					return Direction::DOWN;
+				}
+			}
+			else if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
+			{
+				napravlenie = 3;
+				return Direction::LEFT;
+			}
+			else
+			{
+				napravlenie = 4;
+				return Direction::UP;
+			}
+		}
+		case 3: 
+		{
+			if ((current_status.down == BlockType::FREE) | (current_status.down == BlockType::EXIT))
+			{
+				if (current_status.left == BlockType::EXIT)
+				{
+					return Direction::LEFT;
+				}
+				else if (current_status.up == BlockType::EXIT)
+				{
+					return Direction::UP;
+				}
+				else
+				{
+					napravlenie = 2;
+					return Direction::DOWN;
+				}
+			}
+			else if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
+			{
+				if (current_status.up == BlockType::EXIT)
+				{
+					return Direction::UP;
+				}
+				else
+				{
+					return Direction::LEFT;
+				}
+			}
+			else if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))
+			{
+				napravlenie = 4;
+				return Direction::UP;
+			}
+			else
+			{
+				napravlenie = 1;
+				return Direction::RIGHT;
+			}
+		}
+		default: 
+		{
+			if ((current_status.left == BlockType::FREE) | (current_status.left == BlockType::EXIT))
+			{
+				if (current_status.up == BlockType::EXIT)
+				{
+					return Direction::UP;
+				}
+				else if (current_status.right == BlockType::EXIT)
+				{
+					return Direction::RIGHT;
+				}
+				else
+				{
+					napravlenie = 3;
+					return Direction::LEFT;
+				}
+			}
+			else if ((current_status.up == BlockType::FREE) | (current_status.up == BlockType::EXIT))
+			{
+				if (current_status.right == BlockType::EXIT)
+				{
+					return Direction::RIGHT;
+				}
+				else
+				{
+					return Direction::UP;
+				}
+			}
+			else if ((current_status.right == BlockType::FREE) | (current_status.right == BlockType::EXIT))
+			{
+				napravlenie = 1;
+				return Direction::RIGHT;
+			}
+			else
+			{
+				napravlenie = 2;
+				return Direction::DOWN;
+			}
+		}
+	}
 }
