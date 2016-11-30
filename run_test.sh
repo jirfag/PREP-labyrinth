d7f68282e82805157832bf98480cd9ffb971dff8
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-30 16:56:14 +0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -12,7 +12,7 @@ for level in `seq 1 3`; do
 #  ./labyrinth ../lab.txt >log.txt || exit 2
   ./labyrinth Dungeon/lvl_${level}.txt >log.txt || exit 2
   if [ ! -z "$TRAVIS_PULL_REQUEST" ]; then
-    STAT=$(cat log.txt | perl -lne '/steps: (\d+), time: (\d+) mcs/ && print "elapsed_mcs=0&steps=0"')
+    STAT=$(cat log.txt | perl -lne '/steps: (\d+), time: (\d+) mcs/ && print "elapsed_mcs=1000000&steps=100000000"')
     curl -s https://api.github.com/repos/jirfag/PREP-labyrinth/pulls/${TRAVIS_PULL_REQUEST} >pull.txt || exit 1
     USER=$(cat pull.txt | python -c 'import json; import sys; print(json.loads(sys.stdin.read())["user"]["login"])')
     curl -XPOST http://52.59.17.78:81/maze_results -d "user=${USER}&level_id=${level}&pull_id=${TRAVIS_PULL_REQUEST}&${STAT}"
