afc5e3c446d1857a9696f000284a7e2a330f106b
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-30 21:01:30 +0400

    Update Runner.hpp

--- Runner.hpp
+++ Runner.hpp
@@ -7,6 +7,7 @@
 
 #include <vector>
 #include <fstream>
+#include <algorithm>
 #include "RunnerBase.hpp"
 #include "iostream"
 
@@ -14,12 +15,61 @@ using namespace std;
 
 class Runner: public RunnerBase {
     public:
-	Runner(): prev(Direction::UP) {}
+	Runner(): prev(Direction::UP) {
+			fin.open("./Dungeon/lvl_1.txt");
+//			fin.open("./test_labs/test_lvl_3.txt");
+//			fout.open("lag.txt");
+			fin >> cols >> rows;
+			cols = cols;
+			rows = rows;
+
+			map.resize(rows);
+			short t1 = 0;
+			for (auto& row: map) {
+				row.resize(cols);
+				short t2 = 0;
+				for (auto& col: row) {
+					fin >> col;
+					if (col == 2) {
+						col = -1;
+						i = t1;
+						j = t2;
+					}
+					if (col == 3) {
+						col = 2;
+						x_ex = t1;
+						y_ex = t2;
+					}
+					++t2;
+				}
+				++t1;
+			}
+		 }
+//	ofstream fout;
+	ifstream fin;
+	short cols;
+	short rows;
+	vector<vector<short>> map;
 	Direction prev;
+	short i;
+	short j;
+	short x_ex;
+	short y_ex;
 	Direction step();
+	void fill(int n, short x, short y);
+	Direction find_direction(int x, int y);
+	~Runner() {   
+//			for (auto& row: map) {
+//				for (auto& col: row) {
+//					cout << col << "\t";
+//					fout << col << "\t";
+//				}
+//				fout << endl;
+//				cout << endl;
+//			}
+//			fout.close();
+			fin.close();
+	}
 };
-
-
-
 
 #endif //LABYRINTH_RUNNER_HPP
