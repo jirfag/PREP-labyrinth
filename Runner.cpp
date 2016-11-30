c4451e9eb4f2a306bcab4dbebfa19be41bfd18f4
AndersRichter <andreysav96@rambler.ru>
2016-11-30 06:51:55 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -273,65 +273,64 @@ Direction Runner::step()
 	
 	
 	
-	
 if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == L)&&(current_status.up == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == R)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == R)&&(current_status.down == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == R)&&(current_status.down == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.right == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == U)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == U)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
 			return Direction::UP;
 		}
 	if ( (napravlenie == U)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.left == BlockType::FREE) )
-		if (i == 0)
+		if (i == 3)
 		{
 			napravlenie = U;
 			i++;
