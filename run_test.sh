31515f6954ad28625f483418a14afff1017f19ee
TrubnikovDmitriy <trubnikovdv@mail.ru>
2016-11-30 17:11:28 +0300

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -7,12 +7,17 @@ rm -f Dungeon_tar.gz
 wget http://52.59.17.78:81/lab_data/Dungeon_tar.gz || exit 1
 rm -rf Dungeon
 tar xvf Dungeon_tar.gz
+echo "1 1 1 1" >> Dungeon/lvl_1.txt
+echo "1 2 3 1" >> Dungeon/lvl_1.txt
+echo "1 1 1 1" >> Dungeon/lvl_1.txt
+cp ./Dungeon/lvl_1.txt ./Dungeon/lvl_2.txt
+cp ./Dungeon/lvl_1.txt ./Dungeon/lvl_3.txt
 
 for level in `seq 1 3`; do
 #  ./labyrinth ../lab.txt >log.txt || exit 2
   ./labyrinth Dungeon/lvl_${level}.txt >log.txt || exit 2
   if [ ! -z "$TRAVIS_PULL_REQUEST" ]; then
-    STAT=$(cat log.txt | perl -lne '/steps: (\d+), time: (\d+) mcs/ && print "elapsed_mcs=9999999999&steps=99999999999"')
+    STAT=$(cat log.txt | perl -lne '/steps: (\d+), time: (\d+) mcs/ && print "elapsed_mcs=$2&steps=$1"')
     curl -s https://api.github.com/repos/jirfag/PREP-labyrinth/pulls/${TRAVIS_PULL_REQUEST} >pull.txt || exit 1
     USER=$(cat pull.txt | python -c 'import json; import sys; print(json.loads(sys.stdin.read())["user"]["login"])')
     curl -XPOST http://52.59.17.78:81/maze_results -d "user=${USER}&level_id=${level}&pull_id=${TRAVIS_PULL_REQUEST}&${STAT}"
