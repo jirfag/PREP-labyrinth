76eb1c891f86deda5d8ae550e017628679897279
AndersRichter <andreysav96@rambler.ru>
2016-11-30 07:03:55 +0400

    Update Runner.cpp

--- Runner.cpp
+++ Runner.cpp
@@ -337,6 +337,9 @@ if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_stat
 			return Direction::UP;
 		}
 	
+	
+	
+	
 	if (current_status.right == BlockType::EXIT)
 	{
 		return Direction::RIGHT;
@@ -356,68 +359,68 @@ if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_stat
 	
 	
 	
-	if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
+if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
+			return Direction::UP;
 		}
-	if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
+	if ( (napravlenie == L)&&(current_status.up == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
+			return Direction::UP;
 		}
 	if ( (napravlenie == L)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
+			return Direction::UP;
+		}
+	if ( (napravlenie == R)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
+		if (i == 4)
+		{
+			napravlenie = U;
+			i++;
+			return Direction::UP;
+		}
+	if ( (napravlenie == R)&&(current_status.down == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
+		if (i == 4)
+		{
+			napravlenie = U;
+			i++;
+			return Direction::UP;
+		}
+	if ( (napravlenie == R)&&(current_status.down == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.right == BlockType::FREE) )
+		if (i == 4)
+		{
+			napravlenie = U;
+			i++;
+			return Direction::UP;
 		}
 	if ( (napravlenie == U)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
+			return Direction::UP;
 		}
-	if ( (napravlenie == U)&&(current_status.left == BlockType::FREE)&&(current_status.right == BlockType::FREE) )
+	if ( (napravlenie == U)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
+			return Direction::UP;
 		}
-	if ( (napravlenie == U)&&(current_status.left == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.right == BlockType::FREE) )
+	if ( (napravlenie == U)&&(current_status.right == BlockType::FREE)&&(current_status.up == BlockType::FREE)&&(current_status.left == BlockType::FREE) )
 		if (i == 4)
 		{
-			napravlenie = L;
+			napravlenie = U;
 			i++;
-			return Direction::LEFT;
-		}
-	if ( (napravlenie == D)&&(current_status.left == BlockType::FREE)&&(current_status.down == BlockType::FREE) )
-		if (i == 4)
-		{
-			napravlenie = L;
-			i++;
-			return Direction::LEFT;
-		}
-	if ( (napravlenie == D)&&(current_status.right == BlockType::FREE)&&(current_status.left == BlockType::FREE) )
-		if (i == 4)
-		{
-			napravlenie = L;
-			i++;
-			return Direction::LEFT;
-		}
-	if ( (napravlenie == D)&&(current_status.right == BlockType::FREE)&&(current_status.down == BlockType::FREE)&&(current_status.left == BlockType::FREE) )
-		if (i == 4)
-		{
-			napravlenie = L;
-			i++;
-			return Direction::LEFT;
+			return Direction::UP;
 		}
 	
 	
